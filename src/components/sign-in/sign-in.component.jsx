c4b6d41dd094c7cbcb3146f548ec6c542a08d7c2
Bao Pham <ryanvuepham@gmail.com>
2020-04-28 23:57:00 -0500

    adding Google auth to sigin page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
+import { signInWithGoogle } from "../../firebase/firebase.utils.js";
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -52,6 +53,9 @@ class SignIn extends React.Component {
             required
           />
           <CustomButton type="submit">Sig In</CustomButton>
+          <CustomButton onClick={signInWithGoogle}>
+            Sig in with Google
+          </CustomButton>
         </form>
       </div>
     );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBUzRer6TgU-Q7c_CrE7IOr3Z_4nn-bDiU",
+  authDomain: "ecommerce-db-1bb80.firebaseapp.com",
+  databaseURL: "https://ecommerce-db-1bb80.firebaseio.com",
+  projectId: "ecommerce-db-1bb80",
+  storageBucket: "ecommerce-db-1bb80.appspot.com",
+  messagingSenderId: "711117188118",
+  appId: "1:711117188118:web:4203a2101fcfa852c35937",
+  measurementId: "G-46FB017HM9",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+// will trigger Google sigin popup to select which account to sign in
+provider.setCustomParameters({ prompt: "select_account" });
+// Just want sigin with Google auth. There are others facebook, github etc
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
