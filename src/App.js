b9d169627c416d93730a243d921bb61067e70671
Bao Pham <ryanvuepham@gmail.com>
2020-05-07 22:45:55 -0500

    create user sign in with email and password

--- src/App.js
+++ src/App.js
@@ -32,10 +32,11 @@ class App extends React.Component {
               ...snapShot.data()
             
             }
-             
-          // }, () => {
-          //   console.log(this.state)
           })
+          console.log(this.state)  
+          
+          
+         
         })
         
       }
